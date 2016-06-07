4f4e09a6296079498ac0ef710d4a1f2694c4e028
Anre La <anrelapro@gmail.com>
2016-06-07 11:36:13 +0300

    Create 98_9.js

---
+++ 98_9.js
@@ -0,0 +1,13 @@
+var portrait1 = document.querySelector('.portrait-1');
+portrait1.classList.remove('stylize');
+
+var portrait2 = document.querySelector('.portrait-2');
+portrait2.classList.add('shadow');
+
+var portrait3 = document.querySelector('.portrait-3');
+portrait3.classList.remove('invert');
+portrait3.classList.add('colorize');
+
+var portrait4 = document.querySelector('.portrait-4');
+portrait4.classList.remove('spoil');
+portrait4.classList.add('burst');
